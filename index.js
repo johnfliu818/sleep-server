82d7e2449c8398fc6afe5433dd0363afafe740ba
johnliu <john.liu@karmadata.com>
2017-09-08 16:10:39 -0400

    working now

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http')
+
+function start(port) {
+    function handleRequest(req, resp) {
+        // console.log(req.url)
+        if (req.url != null && req.url.startsWith('/sleep/')) {
+            var timeout = parseInt(req.url.substring(7))
+            setTimeout(function() {
+                resp.end(JSON.stringify({
+                    success: true,
+                    url: req.url,
+                    timeout: timeout
+                }))
+            }, timeout)
+        } else {
+            resp.statusCode = 404
+            resp.end(JSON.stringify({success: false}))
+        }
+    }
+
+    var server = http.createServer(handleRequest)
+    server.listen(port, function() {
+        console.log("Server listening on http://localhost:%s", port)
+    })
+}
+
+exports.start = start
---
+++ test.js
@@ -0,0 +1,3 @@
+var sleepserver = require('./index.js')
+
+sleepserver.start(3500)
