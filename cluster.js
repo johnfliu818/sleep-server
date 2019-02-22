c3e4bbdbef4633b787588b44c0793a5dcf442bac
highmountaintea <jlpub@coolimage.com>
2019-02-22 12:46:39 -0500

    added clustering

--- cluster.js
+++ cluster.js
@@ -1,19 +1,25 @@
-const cluster = require('cluster');
+#!/usr/bin/env node
 
-const clusterSize = parseInt(process.argv[3]);
+var cluster = require('cluster');
 
-function forkProcess() {
-  let worker = cluster.fork();
-}
-
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < clusterSize; i++) {
-    forkProcess();
-  }
+if (process.argv.length < 4) {
+  console.log('usage: sleep-server-cluster <port> <clusterSize>');
 } else {
-  require('./server');
-  console.log(`Worker ${process.pid} started`);
+  var clusterSize = parseInt(process.argv[3]);
+
+  function forkProcess() {
+    let worker = cluster.fork();
+  }
+
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < clusterSize; i++) {
+      forkProcess();
+    }
+  } else {
+    require('./server');
+    console.log(`Worker ${process.pid} started`);
+  }
 }
